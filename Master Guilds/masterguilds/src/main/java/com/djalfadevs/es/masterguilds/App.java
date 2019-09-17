166305c3f0cc69d5a5743bbd5b4dfe5a8005bf55
lmml-l <43039968+lmml-l@users.noreply.github.com>
2019-09-17 10:42:43 +0200

    App Class
    Como no empece el proyecto como uno de Spring la App no estaba bien puesta para lanzar el servidor , me he basado en el codigo que hice para la pokedex para crear la base

--- Master Guilds/masterguilds/src/main/java/com/djalfadevs/es/masterguilds/App.java
+++ Master Guilds/masterguilds/src/main/java/com/djalfadevs/es/masterguilds/App.java
@@ -1,13 +1,15 @@
 package com.djalfadevs.es.masterguilds;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication.run(App.class, args);
     }
 }
