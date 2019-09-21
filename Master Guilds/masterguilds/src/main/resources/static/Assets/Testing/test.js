a8aed4e229ec6887d1cdeaad3f87a7e3dbd68733
angel <43203190+aaangell@users.noreply.github.com>
2019-09-21 14:38:52 +0200

    Test 21-09
    Added FunctionList

--- Master Guilds/masterguilds/src/main/resources/static/Assets/Testing/test.js
+++ Master Guilds/masterguilds/src/main/resources/static/Assets/Testing/test.js
@@ -3,7 +3,7 @@
 $(document).ready( function () {
 
 var testAbilitie = new Abilitie({ID: 1 , name: "BUFFSTATS III" , baseActiveTurns: 3 , baseChargeTurns: 7 ,
- description: "Erase una vez una prueba", remainChargeTurns: 7 , isReady: false})
+ description: "Erase una vez una prueba", remainChargeTurns: 7 , isReady: false,effectFunction:effectFunctionList1})
 
 //console.log(testAbilitie.isReady);
 
@@ -29,20 +29,20 @@ var testHero = new Hero({ID: 1 , name: "PRUEBA" , baseAttack: 100, attack: 100, 
 		defence: 100, baseHP: 100,HP: 100,base_crit_hit_chance: 0.20,
 		crit_hit_chance: 0.20 ,description: "Erase una vez en Test",evasion: 50,baseEvasion: 50,
 		abilities: [testAbilitie], image_url: "url" , activeAbilities: [] , aggro: 100 , faction: "1",
-		rarity: 3, level: 20 , exp: 1000 , role: "useless only info provide by attack and defence" })
+		rarity: 3, level: 20 , exp: 1000 , role: "useless only info provide by attack and defence"})
 
 //console.log(testHero.toString());
 
 testHero.abilities[0].useAbilitie(testHero);//Aplicar efecto
 
 testHero.nextTurn();//Actualizacion de heroe
-console.log(testHero);
+process.stdout.write(JSON.stringify(testHero) + '\n');
+testHero.nextTurn();//Actualizacion de heroe 
+console.dir(testHero);
 testHero.nextTurn();//Actualizacion de heroe
-console.log(testHero);
+console.dir(testHero);
 testHero.nextTurn();//Actualizacion de heroe
-console.log(testHero);
-testHero.nextTurn();//Actualizacion de heroe
-console.log(testHero);
+console.dir(testHero);
 
 //console.log(testHero);
 
