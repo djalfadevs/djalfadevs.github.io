a1fdbdf590b59269a377d15bb30b0c1a39f18655
lmml-l <43039968+lmml-l@users.noreply.github.com>
2019-10-11 15:54:14 +0200

    CollectionScene continued
    Seguimos trabajando en la collection scene

--- Master Guilds/masterguilds/src/main/resources/static/Assets/Scripts/Scenes/collectionScene.js
+++ Master Guilds/masterguilds/src/main/resources/static/Assets/Scripts/Scenes/collectionScene.js
@@ -11,7 +11,19 @@ class collection extends Phaser.Scene{
             numberOfPages:0,
             numberOfPage:0,
             numberOfPageText:null,
-            bigcardSprite:null
+            bigcardSprite:null,
+            text:{
+                name: null,
+                lore: null,
+                attack: null,
+                defense: null,
+                Hp: null,
+                evasion: null,
+                aggro: null,
+                rarity: null,
+                crit_hit_chance: null,
+                abilities: null,
+            }
         }
     }
     preload(){
@@ -26,21 +38,32 @@ class collection extends Phaser.Scene{
     	that.extend.numberOfPageText = this.add.text(300,300,that.extend.numberOfPage);
         var numberOfPagesText = this.add.text(320,320,that.extend.numberOfPages);
 
+        //Textos
+        this.extend.text.name = this.add.text(200,200,"").setDepth(1);
+        this.extend.text.lore = this.add.text(200,200,"",{wordWrap:{width:200}}).setDepth(1);
+        this.extend.text.attack = this.add.text(200,200,"").setDepth(1);
+        this.extend.text.defense = this.add.text(200,200,"").setDepth(1);
+        this.extend.text.Hp = this.add.text(200,200,"").setDepth(1);
+        this.extend.text.evasion = this.add.text(200,200,"").setDepth(1);
+        this.extend.text.aggro = this.add.text(200,200,"").setDepth(1);
+        this.extend.text.rarity = this.add.text(200,200,"").setDepth(1);
+        this.extend.text.crit_hit_chance = this.add.text(200,200,"").setDepth(1);
+        this.extend.text.abilities = this.add.text(200,200,"").setDepth(1);
 
     	//BOTONES
     	//Boton volver
-        var backButt=this.add.sprite(100,100,'backButt').setScale(0.5).setInteractive();
+        var backButt=this.add.sprite(100,100,'backButt').setScale(1).setInteractive();
         backButt.on('pointerdown',function(){this.setFrame(1);transition("back",that)})
         backButt.on('pointerup',function(){this.setFrame(0)})
 
-        var UpArrowButt=this.add.sprite(200,100,'UpArrow').setScale(0.5).setInteractive();
+        var UpArrowButt=this.add.sprite(1350,100,'UpArrow').setScale(1).setInteractive();
         UpArrowButt.on('pointerup',function(){
             that.extend.numberOfPage=(that.extend.numberOfPage+1)%that.extend.numberOfPages;
             that.extend.numberOfPageText.setText(that.extend.numberOfPage);
             that.drawCards(that.extend.numberOfPage)
             ;})
 
-        var DownArrowButt=this.add.sprite(200,200,'DownArrow').setScale(0.5).setInteractive();
+        var DownArrowButt=this.add.sprite(1350,1000,'DownArrow').setScale(1).setInteractive();
         DownArrowButt.on('pointerup',function(){
             that.extend.numberOfPage-=1
             if(that.extend.numberOfPage<0){
@@ -70,7 +93,7 @@ class collection extends Phaser.Scene{
 
     	//Se crean las cartas
     	var collsDistance = 250
-    	var rowsDistance = 300
+    	var rowsDistance = 250
     	var allHeroes = game.global.user.heros;
     	
 
@@ -78,7 +101,7 @@ class collection extends Phaser.Scene{
     		if(that.extend.cards[j]!=null)
     		that.extend.cards[j].destroy();
     		if(allHeroes[i*9+j]!=null)
-    		that.extend.cards[j] = new CollectionCard(this,1100+(j%3)*collsDistance,200+(Math.floor((j/3))%3)*rowsDistance,allHeroes[i*9+j],400,400);
+    		that.extend.cards[j] = new CollectionCard(this,1100+(j%3)*collsDistance,300+(Math.floor((j/3))%3)*rowsDistance,allHeroes[i*9+j],400,400);
     	}
     	
     	
--- Master Guilds/masterguilds/target/classes/static/Assets/Scripts/Graphic_Classes/collectionCard.js
+++ Master Guilds/masterguilds/target/classes/static/Assets/Scripts/Graphic_Classes/collectionCard.js
@@ -17,6 +17,17 @@ var CollectionCard = new Phaser.Class({
 		this.cardContainer.setPosition(x,y)
 
 		this.HeroSprite.on('pointerdown',function(){
+			scene.extend.text.name.setText(that.hero.name);
+            scene.extend.text.lore.setText(that.hero.description[0]);
+            scene.extend.text.attack.setText(that.hero.baseAttack)
+            scene.extend.text.defense.setText(that.hero.baseDefence)
+            scene.extend.text.Hp.setText(that.hero.baseHP);
+            scene.extend.text.evasion.setText(that.hero.baseEvasion)
+            scene.extend.text.rarity.setText(that.hero.rarity)
+            scene.extend.text.crit_hit_chance.setText(that.hero.base_crit_hit_chance)
+            scene.extend.text.abilities.setText(that.hero.abilities[0].name)
+
+            scene.extend.bigcardSprite = scene.add.sprite(that.xG,that.yG,'azon_big_card_front');
 			if(scene.extend.bigcardSprite !=null)
 			scene.extend.bigcardSprite.destroy();
 			scene.extend.bigcardSprite = scene.add.sprite(that.xG,that.yG,'azon_big_card_front');
