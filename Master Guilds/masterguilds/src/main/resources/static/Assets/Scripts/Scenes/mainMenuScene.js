067b4fa44ca579c28b16bc09881e3c328d67cc1c
Alvaro Garcia <digifireuni@gmail.com>
2019-10-11 14:29:16 +0200

    Añadida zona noticias a main menu

--- Master Guilds/masterguilds/src/main/resources/static/Assets/Scripts/Scenes/mainMenuScene.js
+++ Master Guilds/masterguilds/src/main/resources/static/Assets/Scripts/Scenes/mainMenuScene.js
@@ -20,12 +20,15 @@ class mainMenu extends Phaser.Scene{
         
         //TEMP BUTTON PLACEMENTS
         
-        var historyplayButt=this.add.sprite(650,250,'largePlayHistoryButt').setInteractive()
-        var misionsplayButt=this.add.sprite(650,550,'largePlayMisionsButt').setInteractive()
-        var arenaplayButt=this.add.sprite(650,850,'largePlayArenaButt').setInteractive()    
-        var collButt=this.add.sprite(1450,550,'largeButt').setInteractive()
-        var shopButt=this.add.sprite(1450,850,'largeAdministrationButt').setInteractive()
-        var backButt=this.add.sprite(100,100,'backButt').setScale(0.5).setInteractive()
+        var historyplayButt=this.add.sprite(650,350,'largePlayHistoryButt').setInteractive()
+        var misionsplayButt=this.add.sprite(650,650,'largePlayMisionsButt').setInteractive()
+        var arenaplayButt=this.add.sprite(650,950,'largePlayArenaButt').setInteractive()    
+        var collButt=this.add.sprite(1450,650,'largeButt').setInteractive()
+        var shopButt=this.add.sprite(1450,950,'largeAdministrationButt').setInteractive()
+        var backButt=this.add.sprite(100,100,'backButt').setInteractive()
+        
+        var news=this.add.sprite(1450,250,'mediumInfo').setInteractive()
+
         
         backButt.on('pointerdown',function(){this.setFrame(1);transition("back",that)})
         backButt.on('pointerup',function(){this.setFrame(0)})
@@ -55,11 +58,11 @@ class mainMenu extends Phaser.Scene{
         backButt.on('pointerout',function(){this.setFrame(0)});
 
         
-        this.add.text(550,205,'Story',{fontFamily:"Museo-700" ,fontSize:'69px',color:'#000',fontStyle:'bold'})
-        this.add.text(515,505,'Missions',{fontFamily:"Museo-700" ,fontSize:'69px',color:'#000',fontStyle:'bold'})
-        this.add.text(550,805,'Arena',{fontFamily:"Museo-700" ,fontSize:'69px',color:'#000',fontStyle:'bold'})
-        this.add.text(1250,505,'Collection',{fontFamily:"Museo-700" ,fontSize:'69px',color:'#000',fontStyle:'bold'})
-        this.add.text(1350,805,'Shop',{fontFamily:"Museo-700" ,fontSize:'69px',color:'#000',fontStyle:'bold'})
+        this.add.text(550,305,'Story',{fontFamily:"Museo-700" ,fontSize:'69px',color:'#000',fontStyle:'bold'})
+        this.add.text(515,605,'Missions',{fontFamily:"Museo-700" ,fontSize:'69px',color:'#000',fontStyle:'bold'})
+        this.add.text(550,905,'Arena',{fontFamily:"Museo-700" ,fontSize:'69px',color:'#000',fontStyle:'bold'})
+        this.add.text(1250,605,'Collection',{fontFamily:"Museo-700" ,fontSize:'69px',color:'#000',fontStyle:'bold'})
+        this.add.text(1350,905,'Shop',{fontFamily:"Museo-700" ,fontSize:'69px',color:'#000',fontStyle:'bold'})
 
         //pointerOverFunctions [COPYPASTED LOGIN]
     
