043b80c7b8e53550baa4017bd7083e3aa9ce7f34
lmml-l <43039968+lmml-l@users.noreply.github.com>
2019-10-14 15:33:35 +0200

    Inicio Logica deck carta
    Cambio base en collectionCard para aproximar su funcionamiento en la escena de deck

--- Master Guilds/masterguilds/src/main/resources/static/Assets/Scripts/Graphic_Classes/collectionCard.js
+++ Master Guilds/masterguilds/src/main/resources/static/Assets/Scripts/Graphic_Classes/collectionCard.js
@@ -16,22 +16,45 @@ var CollectionCard = new Phaser.Class({
 		this.cardContainer = scene.add.container(0,0,[this.HeroSprite]);
 		this.cardContainer.setPosition(x,y)
 
-		this.HeroSprite.on('pointerdown',function(){
-			scene.extend.text.name.setText(that.hero.name);
-            scene.extend.text.lore.setText(that.hero.description[0]);
-            scene.extend.text.attack.setText(that.hero.baseAttack)
-            scene.extend.text.defense.setText(that.hero.baseDefence)
-            scene.extend.text.Hp.setText(that.hero.baseHP);
-            scene.extend.text.evasion.setText(that.hero.baseEvasion)
-            scene.extend.text.rarity.setText(that.hero.rarity)
-            scene.extend.text.crit_hit_chance.setText(that.hero.base_crit_hit_chance)
-            scene.extend.text.abilities.setText(that.hero.abilities[0].name)
+		//En el caso del inventario de cartas
+		if(that.scene.scene.key=="collection"){
+			that.HeroSprite.on('pointerdown',function(){
+			that.scene.extend.text.name.setText(that.hero.name);
+            that.scene.extend.text.lore.setText(that.hero.description[0]);
+            that.scene.extend.text.attack.setText(that.hero.baseAttack)
+            that.scene.extend.text.defense.setText(that.hero.baseDefence)
+            that.scene.extend.text.Hp.setText(that.hero.baseHP);
+            that.scene.extend.text.evasion.setText(that.hero.baseEvasion)
+            that.scene.extend.text.rarity.setText(that.hero.rarity)
+            that.scene.extend.text.crit_hit_chance.setText(that.hero.base_crit_hit_chance)
+            that.scene.extend.text.abilities.setText(that.hero.abilities[0].name)
 
-            scene.extend.bigcardSprite = scene.add.sprite(that.xG,that.yG,'azon_big_card_front');
-			if(scene.extend.bigcardSprite !=null)
-			scene.extend.bigcardSprite.destroy();
-			scene.extend.bigcardSprite = scene.add.sprite(that.xG,that.yG,'azon_big_card_front');
+            that.scene.extend.bigcardSprite = scene.add.sprite(that.xG,that.yG,'azon_big_card_front');
+			if(that.scene.extend.bigcardSprite !=null)
+			that.scene.extend.bigcardSprite.destroy();
+			that.scene.extend.bigcardSprite = scene.add.sprite(that.xG,that.yG,'azon_big_card_front');
 		})
+		}
+		//En el caso del deck despues de seleccionar mision
+		else if(that.scene.scene.key=="deck"){
+			that.HeroSprite.on('pointerdown',function(){
+			that.scene.extend.text.name.setText(that.hero.name);
+            that.scene.extend.text.lore.setText(that.hero.description[0]);
+            that.scene.extend.text.attack.setText(that.hero.baseAttack)
+            that.scene.extend.text.defense.setText(that.hero.baseDefence)
+            that.scene.extend.text.Hp.setText(that.hero.baseHP);
+            that.scene.extend.text.evasion.setText(that.hero.baseEvasion)
+            that.scene.extend.text.rarity.setText(that.hero.rarity)
+            that.scene.extend.text.crit_hit_chance.setText(that.hero.base_crit_hit_chance)
+            that.scene.extend.text.abilities.setText(that.hero.abilities[0].name)
+
+            that.scene.extend.bigcardSprite = scene.add.sprite(that.xG,that.yG,'azon_big_card_front');
+			if(that.scene.extend.bigcardSprite !=null)
+			that.scene.extend.bigcardSprite.destroy();
+			that.scene.extend.bigcardSprite = scene.add.sprite(that.xG,that.yG,'azon_big_card_front');
+		})
+		}
+	
 	},
 	destroy(){
 		this.HeroSprite.destroy();
