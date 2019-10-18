354005a5f36707bd09e236ae2e5204e2f3fb992b
lmml-l <43039968+lmml-l@users.noreply.github.com>
2019-10-18 23:11:34 +0200

    Arena scene

---
+++ Master Guilds/masterguilds/src/main/resources/static/Assets/Scripts/Scenes/arena.js
@@ -0,0 +1,17 @@
+class arena extends Phaser.Scene{
+	constructor(){
+		super({key:"arena"})
+		this.extend = {
+			click:null
+		}
+	}
+	create(){
+		var that=this;
+		var background = this.add.sprite(960,280,'backWood');
+		this.extend.click=this.sound.add('click');
+    	this.extend.click.setVolume(game.global.user.evol)
+		var backButt=this.add.sprite(85,80,'backButt').setInteractive()          
+        backButt.on('pointerdown',function(){this.setFrame(1)})
+        backButt.on('pointerup',function(){this.setFrame(0);that.scene.transition({target:'mainMenu',duration:0})})
+	}
+}
